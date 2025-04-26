a8e24e38696649d0fa374e8c2d3e04a87bf88692
egboyguda <gboyguda@gmail.com>
2025-04-26 21:51:28 +0800

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-400">Hello</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>click</Button>
+        </div>
+        <div>
+          <Input placeholder="i am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="im a text area" />
+        </div>
+      </div>
     </div>
   );
 }
